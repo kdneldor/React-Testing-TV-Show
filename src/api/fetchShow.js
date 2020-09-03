617bd639d20f03ddf62cc109a3a9b96a5f10a398
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-09-02 23:50:25 -0500

    Setup test for Episodes complete, moving on to App test

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
-
 const fetchShow = () => {
   return axios
     .get(
       "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
     )
     .then((res) => {
-        console.log(res.data)
-      return res.data;
+      console.log(res.data);
+      return res;
     });
 };
 
